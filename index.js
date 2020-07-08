73066886de972f949047d2fce941bbf9bfb9e59f
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2020-07-08 10:43:58 +0200

    delete http.createserver function

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 const morgan = require("morgan");
 const http = require('http');
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Welcome to my film app!\n');
-}).listen(8080);
 
 app.use(express.static("public"));
 app.use(morgan("common"));
@@ -65,6 +61,6 @@ app.get('/films', (req, res) => {
   res.json(topFilms);
 });
 
-});
-
+app.listen(8080, () => {
 console.log('My first Node test server is running on Port 8080.');
+});
