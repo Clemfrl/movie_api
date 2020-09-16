3f4d3603ee09d2d87beeac698435546be25446e5
root <root@DESKTOP-4NLJGLM.localdomain>
2020-09-16 17:42:05 +0200

    connecting URI

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Users = Models.User;
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 const cors = require('cors');
