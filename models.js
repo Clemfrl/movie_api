2347c4fbbd6e3836ab8a5ff7ce80b575ca962d3c
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2020-09-25 13:11:34 +0200

    delete usershema

--- models.js
+++ models.js
@@ -33,13 +33,7 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
-let userSchema = mongoose.Schema({
-    Username: { type: String, required: true },
-    Password: { type: String, required: true },
-    Email: { type: String, required: true },
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-});
+
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
