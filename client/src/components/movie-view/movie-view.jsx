52132b9c6b6edfffeb61dcb767f073e6a402f65b
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2020-11-09 13:49:41 +0100

    movie view set up

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 export class MovieView extends React.Component {
   constructor() {
@@ -13,25 +16,18 @@ export class MovieView extends React.Component {
     if (!movie) return null;
 
     return (
-      <div className="movie-view">
-        <img className="movie-poster" src={movie.ImagePath} />
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
+      <div>
+        <Card style={{ width: "20rem" }}>
+          <Card.Img variant="top" src={movie.ImagePath} />
+          <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+            <Card.Text>Description: {movie.Description}</Card.Text>
+            <Card.Text>Genre: {movie.Genre.Name}</Card.Text>
+            <Card.Text>Director: {movie.Director.Name}</Card.Text>
+            <Card.Text>Director Bio: {movie.Director.Bio}</Card.Text>
+            <Button variant="link">Back</Button>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
