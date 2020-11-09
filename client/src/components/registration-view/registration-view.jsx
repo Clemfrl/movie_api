496ae6393fdc4dffb02f553aa0276964c9a93f44
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2020-11-09 11:47:56 +0100

    created a registration view

---
+++ client/src/components/registration-view/registration-view.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export function RegistrationView() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [dob, setDob] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const createdUser = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: dob,
+    };
+
+    axios
+      .post("https://clemflixdb.herokuapp.com/users", createdUser)
+      .then((response) => {
+        console.log(response);
+        console.log(response.data);
+        alert("User created successfully");
+        window.open("/client", "_self");
+      })
+      .catch((e) => {
+        console.log(e.reponse);
+        alert("Error processing request");
+      });
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        Email adress:
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Date of Birth:
+        <input
+          type="date"
+          placeholder="22/10/1991"
+          value={dob}
+          onChange={(e) => setDob(e.target.value)}
+        />
+      </label>
+      <button type="button" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
+}
