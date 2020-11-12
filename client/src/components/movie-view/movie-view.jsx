4f030bab9240580d508a81d6fdd9b59c8aef4ae4
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2020-11-12 16:24:46 +0100

    try to display my cards 3 in row

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 export class MovieView extends React.Component {
   constructor() {
@@ -16,18 +19,24 @@ export class MovieView extends React.Component {
     if (!movie) return null;
 
     return (
-      <div>
-        <Card style={{ width: "20rem" }}>
-          <Card.Img variant="top" src={movie.ImagePath} />
-          <Card.Body>
-            <Card.Title>{movie.Title}</Card.Title>
-            <Card.Text>Description: {movie.Description}</Card.Text>
-            <Card.Text>Genre: {movie.Genre.Name}</Card.Text>
-            <Card.Text>Director: {movie.Director.Name}</Card.Text>
+      <div className="container-fluid d-flex justify-content-center">
+        <div className="row">
+          <div className="col-md-4">
+            <Card style={{ width: "20rem" }}>
+              <Card.Img variant="top" src={movie.ImagePath} />
+              <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text>Description: {movie.Description}</Card.Text>
+                <Card.Text>Genre: {movie.Genre.Name}</Card.Text>
+                <Card.Text>Director: {movie.Director.Name}</Card.Text>
 
-            <Button onClick={() => onBackButtonClick(movie)}>Back</Button>
-          </Card.Body>
-        </Card>
+                <Button onClick={() => onBackButtonClick(movie)}>Back</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-4"></div>
+          <div className="col-md-4"></div>
+        </div>
       </div>
     );
   }
