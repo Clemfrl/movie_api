c75f31738ad973e6ada35f375c0119ca61e76900
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2020-11-18 15:06:06 +0100

    Imported kinks for Director and Genre

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -3,30 +3,35 @@ import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+import { Link } from "react-router-dom";
+
 export class MovieCard extends React.Component {
   render() {
     // This is given to the <MovieCard/> component by the outer world
     // which, in this case, is `MainView`, as `MainView` is what’s
     // connected to your database via the movies endpoint of your API
-    const { movie, onClick } = this.props;
+    const { movie } = this.props;
 
     return (
-      <div className="col-10 col-lg-3 ml-5 mt-5">
-        <Card style={{ width: "20rem" }}>
-          <Card.Img variant="top" src={movie.ImagePath} />
-          <Card.Body>
-            <Card.Title>{movie.Title}</Card.Title>
-            <Card.Text>{movie.Description}</Card.Text>
-            <Button onClick={() => onClick(movie)} variant="link">
-              Learn more
-            </Button>
-          </Card.Body>
-        </Card>
-      </div>
+      <Card style={{ width: "20rem" }}>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link">Learn more</Button>
+          </Link>
+          <Link to={`/movies/director/${movie.Director.Name}`}>
+            <Button variant="link">Director</Button>
+          </Link>
+          <Link to={`/movies/genres/${movie.Genre.Name}`}>
+            <Button variant="link">Genre</Button>
+          </Link>
+        </Card.Body>
+      </Card>
     );
   }
 }
-
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     ImagePath: PropTypes.string.isRequired,
