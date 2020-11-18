b26c84d21689959efac8d1ff896e9a0fd6f1039f
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2020-11-18 16:28:10 +0100

    Boostrap styling on Login view/Registerview

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -5,6 +5,7 @@ import "./login-view.scss";
 import logo from "../../images/myFlix.png";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
@@ -29,40 +30,42 @@ export function LoginView(props) {
   };
 
   return (
-    <Form className="login-form">
-      <img src={logo} alt="logo" style={{ width: "300px" }} />
-      <Form.Group controlId="formBasicUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          type="username"
-          placeholder="Enter Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
-      <Button
-        variant="btn-lg btn-dark btn-block"
-        type="submit"
-        onClick={handleSubmit}
-      >
-        Login
-      </Button>
-      <div className="registration">Not registered yet ?</div>
-      <Link to={`/register`}>
-        <Button variant="link" className="registerButton" type="submit">
-          Sign Up
+    <Container className="login-container">
+      <Form className="login-form">
+        <img src={logo} alt="logo" style={{ width: "300px" }} />
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="username"
+            placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button
+          variant="btn-lg btn-dark btn-block"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Login
         </Button>
-      </Link>
-    </Form>
+        <div className="registration">Not registered yet ?</div>
+        <Link to={`/register`}>
+          <Button variant="link" className="registerButton" type="submit">
+            Sign Up
+          </Button>
+        </Link>
+      </Form>
+    </Container>
   );
 }
 
--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import logo from "../../images/myFlix.png";
+import "./registration-view.scss";
 
 export function RegistrationView() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [dob, setDob] = useState("");
+  const [username, createUsername] = useState("");
+  const [password, createPassword] = useState("");
+  const [email, createEmail] = useState("");
+  const [dob, createDob] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,46 +35,62 @@ export function RegistrationView() {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input
-          type="text"
-          placeholder="Enter username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <label>
-        Email adress:
-        <input
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Date of Birth:
-        <input
-          type="date"
-          placeholder="22/10/1991"
-          value={dob}
-          onChange={(e) => setDob(e.target.value)}
-        />
-      </label>
-      <button type="button" onClick={handleSubmit}>
-        Submit
-      </button>
-    </form>
+    <Container className="registration-container">
+      <Form className="registration-form">
+        <img src={logo} alt="logo" style={{ width: "300px" }} />
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => createUsername(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => createPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email adress:</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => createEmail(e.target.value)}
+          />
+
+          <Form.Text className="text-muted">
+            We will never share your information with anyone
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicDob">
+          <Form.Label>Birthday</Form.Label>
+          <Form.Control
+            type="date"
+            placeholder="12/31/1999"
+            value={dob}
+            onChange={(e) => createDob(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check
+            type="checkbox"
+            label="I want to hear the latest news from myFlix"
+          />
+        </Form.Group>
+        <Button type="submit" onClick={handleSubmit}>
+          Register
+        </Button>{" "}
+        <Link to={"/login"}>
+          <Button>Go to Login</Button>
+        </Link>
+      </Form>
+    </Container>
   );
 }
